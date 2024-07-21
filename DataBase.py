e56be1efab30b8c3739f5fbf3c0a9e369310aa95
Mark <galactycraftxxl@gmail.com>
2024-07-21 11:52:02 +0300

    [A] DB Changes - new user functions. [FIX] DB illegal work. [A] server - update energy per second

--- DataBase.py
+++ DataBase.py
@@ -5,132 +5,139 @@ import pymysql
 
 
 class DataBase:
-    @staticmethod
-    def viewAllDb() -> tuple:  # смотрим всю базу данных
+
+    def __init__(self):
+        self.connection = pymysql.connect(  # настраиваем соединение
+            host="localhost",
+            port=3305,
+            user="root",
+            password="root",
+            database="sasmilcombatdb",
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+    def viewAllDb(self) -> dict:  # смотрим всю базу данных
         try:
-            connection = pymysql.connect(
-                host="localhost",
-                port=3305,
-                user="root",
-                password="root",
-                database="sasmilcombatdb",
-                cursorclass=pymysql.cursors.DictCursor
-            )
-            print("successfully connected...")
-            try:
-                with connection.cursor() as cursor:
-                    query = "SELECT * FROM users"
-                    cursor.execute(query)
-                    rows = cursor.fetchall()  # fetchall возвращает все данные из таблицы
-                    return rows
-            finally:
-                connection.close()
+            with self.connection.cursor() as cursor:
+                query = "SELECT * FROM users"  # выбираем всё из пользователей
+                cursor.execute(query)
+                rows = cursor.fetchall()  # fetchall возвращает все данные из query
+                return rows
+        except Exception as e:
+            print("Connection refused...")
+            print(e)
+
+    def checkUser(self, id: int) -> dict:  # проверяем есть ли пользователь в БД по ID
+        try:
+            with self.connection.cursor() as cursor:
+                query = f"SELECT EXISTS(SELECT id FROM users WHERE id = {id})"  # проверяем есть ли пользователь в БД
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                return rows[0]
+        except Exception as ex:
+            print("Connection refused...")
+            print(ex)
+
+    def createUser(self, data: dict) -> bool:  # создаем нового пользователя
+        try:
+            with self.connection.cursor() as cursor:
+                query = f"INSERT INTO `sasmilcombatdb`.`users` (id, money, CPS, energy) VALUES ({data['id']}, {data['money']}, {data['CPS']}, {data['energy']});"
+                cursor.execute(query)
+                self.connection.commit()
+                return True
 
         except Exception as ex:
             print("Connection refused...")
             print(ex)
 
-    @staticmethod
-    def checkUser(id: int) -> tuple:  # проверяем есть ли пользователь в БД по ID
+    def getUserData(self, id: int) -> list:  # Берем данные пользователя по ID
         try:
-            connection = pymysql.connect(
-                host="localhost",
-                port=3305,
-                user="root",
-                password="root",
-                database="sasmilcombatdb",
-                cursorclass=pymysql.cursors.DictCursor
-            )
-            print("successfully connected...")
-            try:
-                with connection.cursor() as cursor:
-                    query = f"SELECT EXISTS(SELECT id FROM users WHERE id = {id})"
-                    cursor.execute(query)
-                    rows = cursor.fetchall()
-                    return rows
-            finally:
-                connection.close()
+            with self.connection.cursor() as cursor:
+                query = f"SELECT * FROM `sasmilcombatdb`.`users` WHERE id = {id};"
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                return rows[0]
 
         except Exception as ex:
             print("Connection refused...")
             print(ex)
 
-    @staticmethod
-    def createUser(data: dict) -> bool:  # создаем нового пользователя
+    def userClick(self, id: int, money: int, energy: int, lastPlayedTime: str) -> bool:  # обрабатываем click пользователя
         try:
-            connection = pymysql.connect(
-                host="localhost",
-                port=3305,
-                user="root",
-                password="root",
-                database="sasmilcombatdb",
-                cursorclass=pymysql.cursors.DictCursor
-            )
-            print("successfully connected...")
-            try:
-                with connection.cursor() as cursor:
-                    query = f"INSERT INTO `sasmilcombatdb`.`users` (id, money, CPS, energy) VALUES ({data['id']}, {data['money']}, {data['CPS']}, {data['energy']});"
-                    cursor.execute(query)
-                    connection.commit()
-                    return True
-            finally:
-                connection.close()
+            with self.connection.cursor() as cursor:
+                query = f"UPDATE `sasmilcombatdb`.`users` SET money = {money} WHERE id = {id};"
+                cursor.execute(query)
+                self.connection.commit()
+
+                query = f"UPDATE `sasmilcombatdb`.`users` SET energy = {energy} WHERE id = {id};"
+                cursor.execute(query)
+                self.connection.commit()
+
+                query = f"UPDATE `sasmilcombatdb`.`users` SET lastPlayedTime = '{lastPlayedTime}' WHERE id = {id};"
+                cursor.execute(query)
+                self.connection.commit()
+
+                return True
+
+        except Exception as ex:
+            print("Connection refused...")
+            print(ex)
+    def addBoost(self, id: int, boost: str) -> bool:  # добавляем BOOST'ы
+        try:
+            with self.connection.cursor() as cursor:
+                currentBoosts = ""
+                if DataBase.getUserData(id)[0]['boosts'] is not None:
+                    currentBoosts = str(DataBase.getUserData(id)[0]['boosts'])
+                query = f"UPDATE `sasmilcombatdb`.`users` SET boosts = '{str(boost + ',' + currentBoosts)}' WHERE id = {id};"
+                cursor.execute(query)
+                self.connection.commit()
+                return True
 
         except Exception as ex:
             print("Connection refused...")
             print(ex)
 
-    @staticmethod
-    def getUserData(id: int) -> tuple:
+    def getUserBoosts(self, id: int) -> tuple:  # получаем Boost'ы пользователя по ID
         try:
-            connection = pymysql.connect(
-                host="localhost",
-                port=3305,
-                user="root",
-                password="root",
-                database="sasmilcombatdb",
-                cursorclass=pymysql.cursors.DictCursor
-            )
-            print("successfully connected...")
-            try:
-                with connection.cursor() as cursor:
-                    query = f"SELECT * FROM `users` WHERE id = '{id}';"
-                    cursor.execute(query)
-                    rows = cursor.fetchall()
-                    return rows
-            finally:
-                connection.close()
+            with self.connection.cursor() as cursor:
+                query = f"SELECT boosts FROM `sasmilcombatdb`.`users` WHERE id = {id};"
+                cursor.execute(query)
+                res = cursor.fetchall()  # записываем сюда результат из БД
+                rows = res[0]['boosts'].split(',')  # разделяем Boost'ы
+                rows.pop()  # удаляем последний элемент списка
+                return rows
 
         except Exception as ex:
             print("Connection refused...")
             print(ex)
 
-    @staticmethod
-    def userClick(id: int, money: int, energy: int) -> tuple:
+    def updateEnergy(self, id: int, energy: int) -> bool:
         try:
-            connection = pymysql.connect(
-                host="localhost",
-                port=3305,
-                user="root",
-                password="root",
-                database="sasmilcombatdb",
-                cursorclass=pymysql.cursors.DictCursor
-            )
-            print("successfully connected...")
-            try:
-                with connection.cursor() as cursor:
-                    query = f"UPDATE `sasmilcombatdb`.`users` SET money = {money} WHERE id = {id};"
-                    cursor.execute(query)
-                    connection.commit()
+            with self.connection.cursor() as cursor:
+                query = f"UPDATE `sasmilcombatdb`.`users` SET energy = {energy} WHERE id = {id};"
+                cursor.execute(query)
+                self.connection.commit()
 
-                    query = f"UPDATE `sasmilcombatdb`.`users` SET energy = {energy} WHERE id = {id};"
-                    cursor.execute(query)
-                    connection.commit()
+                return True
 
-                    rows = cursor.fetchall()
-                    return rows
-            finally:
-                connection.close()
+        except Exception as ex:
+            print("Connection refused...")
+            print(ex)
+
+    def energyPerSecond(self):
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM users")
+                self.connection.commit()
+
+                for (energy) in cursor:
+                    userID = energy['id']
+                    userEnergy = energy['energy']
+
+                    if not (userEnergy > 1000):
+                        DataBase.updateEnergy(userID, userEnergy + 1)
+                print("Energy++")
+                return True
 
         except Exception as ex:
             print("Connection refused...")
--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 import ast
+from time import sleep
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+
 from DataBase import DataBase
 import uvicorn
+from threading import Thread
+from datetime import datetime
 
 db = DataBase()
 app = FastAPI()  # приложение
@@ -22,6 +26,15 @@ app.add_middleware(  # чтобы CORS работал
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+def updateEnergy():  # обновляем энергию всех пользователей раз в секунду
+    while True:
+        db.energyPerSecond()
+        sleep(1)
+
+
+#energyThread = Thread(target=updateEnergy).start()  # создаём новый поток, чтобы сервер мог одновременно выполнять действия
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -42,11 +55,12 @@ async def registerUser(request: Request):
     temp = await request.body()  # получаем данные
     data = ast.literal_eval(temp.decode('utf-8'))  # превращаем данные в string
 
-    for key, value in db.checkUser(int(data))[0].items():  # Есть ли пользователь в БД?
+    for key, value in db.checkUser(int(data)).items():  # Есть ли пользователь в БД?
         isRegister = bool(value)  # Value либо 1 если есть либо 0 если нет
 
     if not isRegister:  # если пользователь не зарегистрирован
-        defaultUserData = {"id": int(data), "money": 0, "CPS": 0, "energy": 1000}  # Записываем данные пользователя для регистрации. int(data) - ID пользователя.
+        defaultUserData = {"id": int(data), "money": 0, "CPS": 0,
+                           "energy": 1000}  # Записываем данные пользователя для регистрации. int(data) - ID пользователя.
         db.createUser(defaultUserData)  # создаём user'a
 
     return "Ok"
@@ -57,17 +71,50 @@ async def getUsers(request: Request):
     temp = await request.body()  # получаем данные
     data = ast.literal_eval(temp.decode('utf-8'))  # превращаем данные в string
 
-    print(db.getUserData(int(data)))
+    userData = db.getUserData(int(data))  # данные пользователя
+    print(userData)
 
-    return db.getUserData(int(data))[0]
+    lastDate = datetime.strptime(str(userData['LastPlayedTime']),
+                                 "%Y-%m-%d %H:%M:%S.%f")  # последнее время когда пользователь зашел
+    currentDate = datetime.now().replace(microsecond=0)  # текущее время
+
+    difference = (currentDate - lastDate).total_seconds()  # разница между временами
+
+    accumulatedEnergy = int(str(difference).rpartition(".")[0])  # энергия, которая накопилась за время отсутствия
+
+    if accumulatedEnergy > (1000 - userData['energy']):  # проверяем есть ли место, чтобы добавить энергию
+        db.updateEnergy(userData['id'], 1000)  # устанавливаем энергию на 1000
+    else:
+        db.updateEnergy(userData['id'], int(accumulatedEnergy + userData['energy']))  # добавляем к существующей энергии накопленную
+
+    return db.getUserData(int(data))
 
 
 @app.post("/click")
 async def click(request: Request):
     print("Connection!")
-    temp = await request.body()
-    data = ast.literal_eval(temp.decode('utf-8'))
-    db.userClick(data['id'], data['money'], data['energy'])
+    temp = await request.body()  # получаем запрос
+    data = ast.literal_eval(temp.decode('utf-8'))  # конвертим в нормальный стринг
+    currentTime = datetime.now()  # получаем текущее время
+    db.userClick(data['id'], data['money'], data['energy'], str(currentTime))  # отправляем запрос о клике в БД
+
     return f"Status : Ok"
+
+
+@app.post("/buy/boost")
+async def buyBoost(request: Request):
+    temp = await request.body()  # получаем данные
+    data = ast.literal_eval(temp.decode('utf-8'))  # превращаем данные в string
+
+    db.addBoost(data['id'], data['name'])  # Добавляем Буст
+
+
+@app.post("/users/getUserBoosts")
+async def getUsers(request: Request):
+    temp = await request.body()  # получаем данные
+    data = ast.literal_eval(temp.decode('utf-8'))  # превращаем данные в string
+
+    return db.getUserBoosts(int(data))
+
 
 # команда для запуска сервера - uvicorn server:app --reload
